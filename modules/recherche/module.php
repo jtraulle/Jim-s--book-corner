67f37ead0e801a130fa173872a4d55ce7dabbf70
Dorine Talfer <dorine.talfer@gmail.com>
2011-12-12 15:01:31 +0100

    mise en form

--- modules/recherche/module.php
+++ modules/recherche/module.php
@@ -7,7 +7,9 @@ class Recherche extends Module{
 
 		$this->set_title("Recherche d'un livre");		
 		$f=new Form("?module=recherche&action=valide","form1");
-		$f->add_text("titre recherché","titre recherché","titre a rechercher:");				
+		$f->add_text("titre recherché","titre recherché","titre :");
+		$f->add_text("auteur recherché","aureur recherché","auteur :");
+		$f->add_select("statut","statut","statut",array("Disponible","Non Disponible"))->set_value("Disponible");
 		$f->add_submit("Valider","sub")->set_value('Valider');		
 
 		$this->tpl->assign("form",$f);	
