25a9267d52435adbca74382af248741b4784ea26
Dorine Talfer <dorine.talfer@gmail.com>
2011-12-17 19:23:13 +0100

    debut recherche

--- modules/recherche/module.php
+++ modules/recherche/module.php
@@ -7,13 +7,15 @@ class Recherche extends Module{
 
 		$this->set_title("Recherche d'un livre");		
 		$f=new Form("?module=recherche&action=valide","form1");
-		$f->add_text("titre recherché","titre recherché","titre :");
-		$f->add_text("auteur recherché","aureur recherché","auteur :");
+		$f->add_text("titre recherché","titre recherché:","titre :");
+		$f->add_text("auteur recherché","auteur recherché:","auteur :");
 		$f->add_select("statut","statut","statut",array("Disponible","Non Disponible"))->set_value("Disponible");
 		$f->add_submit("Valider","sub")->set_value('Valider');		
 
 		$this->tpl->assign("form",$f);	
-		$this->session->form = $f;		
+		$this->session->form = $f;
+
+		$listeLivre = Livre::liste();
 		
 	}
 	public function action_valide(){
