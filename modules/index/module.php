5edcdebb0245ef6fbeb8c0a9dfb63a6f28868df5
Jean Traullé <jtraulle@gmail.com>
2012-10-13 02:12:24 +0200

    Ajout d'une méthode permettant de récupérer et de mettre à jour le hach du dernier commit depuis le dépôt Github.

--- modules/index/module.php
+++ modules/index/module.php
@@ -7,6 +7,19 @@ class Index extends Module{
         $this->tpl->assign("listeLivres",Livre::cinqDerniersLivres());
         $this->tpl->assign("dernierEvenement",Evenement::dernierEvenement());
 	}
+	
+	public function action_recalculsha(){
+		
+        $raw = json_decode(file_get_contents('https://api.github.com/repos/jtraulle/Jim-s--book-corner/commits'));
+        $lastrevid = substr($raw[0]->sha, 0, 10) ;
+        $handle = fopen("templates/lastrevid.tpl", "w+");
+        fwrite($handle, $lastrevid);
+        
+        
+        $this->site->ajouter_message('Le hash de commit a été correctement mis à jour ;)', 4);
+        $this->site->redirect('index');
+        
+	}
 
 }
 ?>
